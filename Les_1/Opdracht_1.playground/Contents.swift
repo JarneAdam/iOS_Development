700a45894f0aa9cf79a314ce6766990e36405062
JarneAdam <jarne.adam@student.vives.be>
2025-09-24 09:28:26 +0200

    Rearanged folders

--- Les_1/Opdracht_1.playground/Contents.swift
+++
@@ -1,5 +0,0 @@
-//Deel 1: The basics
-
-var text = "SWIFT"
-let max = 10
-var average = 0.0
---
+++ Opdracht_1.playground/Contents.swift
@@ -0,0 +1,17 @@
+//Deel 1: The basics
+
+var text = "SWIFT"
+let max = 10
+var average = 0.0
+
+for character in text {
+    print(character)
+}
+
+var naam: (voornaam: String, familienaam: String) = ("Jarne", "Adam")
+
+print(naam.voornaam)
+print(naam.familienaam)
+
+var reversedString: String? = nil
+
